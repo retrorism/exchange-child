511a8c2a183048800752b8fb0ff41d8d1c6c6d9b
Willem Prins | SOMTIJDS <willem@somtijds.nl>
2017-05-18 18:22:56 +0200

    Fixed styles for griditem titles and archive headers

--- parts/content-archive-header.php
+++ parts/content-archive-header.php
@@ -1,31 +1,18 @@
 <header class="article-header archive__header">
 
-	<div class="archive__header__introduction">
+	<div class="archive__header_introduction">
 
 		<div class="archive__title-wrapper">
 
 		<?php if ( is_post_type_archive( 'story' ) || is_post_type_archive( 'participant' ) ) : ?>
 
-			<h1 class="archive__title" itemprop="headline"><?php the_archive_title() ?></h1>
-
-			<?php 
-				global $wp_query;
-				$type = $wp_query->query['post_type'];
-				if ( ! empty( $type ) ) : ?>
-
-					<div class="editorialintro story__editorialintro" itemprop="headline">
-						<div class="paragraph editorialintro__paragraph">
-						<p><?php echo get_option( 'options_' . $type . '_archive_page_introduction' ); ?></p>
-						</div>
-					</div>
-
-			<?php endif; ?>
+				<h1 class="archive__title" itemprop="headline"><?php the_archive_title() ?></h1>
 				
 		<?php elseif ( is_tax() || get_query_var( 'programme-round' ) ) : ?>
 
-			<h1 class="archive__title" itemprop="headline"><?php the_archive_title() ?></h1>
+				<h1 class="archive__title" itemprop="headline"><?php the_archive_title() ?></h1>
 
-			<?php the_archive_description( '<div class="taxonomy-description">', '</div>' );?>
+				<?php the_archive_description( '<div class="taxonomy-description">', '</div>' );?>
 
 		<?php elseif ( is_search() ) : ?>
 
@@ -34,6 +21,27 @@
 		<?php endif; ?>
 
 		</div>
+
+		<?php if ( is_post_type_archive( 'story' ) || is_post_type_archive( 'participant' ) ) : ?>
+
+			<?php 
+				global $wp_query;
+				$type = $wp_query->query['post_type'];
+				if ( ! empty( $type ) ) : ?>
+
+					<div class="editorialintro story__editorialintro" itemprop="headline">
+						
+						<div class="paragraph editorialintro__paragraph">
+						
+							<p><?php echo get_option( 'options_' . $type . '_archive_page_introduction' ); ?></p>
+						
+						</div>
+					
+					</div>
+
+			<?php endif; ?>
+
+		<?php endif; ?>
 
 	</div>
 
