f6079e502c5802d995c5081e35afb38e3dfa1f6d
Willem Prins | SOMTIJDS <willem@somtijds.nl>
2017-05-26 01:02:23 +0200

    re-enqueue frontend scripts

--- assets/functions/enqueue-scripts.php
+++ assets/functions/enqueue-scripts.php
@@ -11,6 +11,10 @@ function exchange_cl_site_scripts() {
 
     wp_dequeue_script( 'site-js');
 
+    wp_enqueue_script( 'foundation-js', get_template_directory_uri() . '/assets/js/foundation.js', array( 'jquery' ), '6.2', true );
+
+    wp_enqueue_script( 'masonry-js', get_template_directory_uri() . '/vendor/masonry/dist/masonry.pkgd.min.js', array(), '', true );
+
     wp_enqueue_script( 'child-theme-site-js', get_stylesheet_directory_uri() . '/assets/js/scripts.js', array( 'jquery','foundation-js', 'masonry-js' ), false, true );
 
 	wp_localize_script( 'child-theme-site-js', 'exchange_ajax', array( 'ajax_url' => admin_url( 'admin-ajax.php' ) ) );
